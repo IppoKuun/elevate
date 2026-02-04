4d752f4d789a731d97c939f919ada7233e03e4ae
Hippolyte <hippolyte.diallo@gmail.com>
2026-02-04 13:00:50 +0100

    feat(cours/page) add props/ SSR functionnality)

--- app/admin/cours/page.tsx
+++ app/admin/cours/page.tsx
@@ -1,9 +1,19 @@
-import { requireStaffRole } from "@/lib/rbac"
+import { prisma } from "@/lib/db/prisma"
+import { getStaffProfile, requireStaffRole } from "@/lib/rbac"
 
-export default async function newCours(){
+export default async function Cours(){
     await requireStaffRole("ADMIN")
+    const {staff} = await getStaffProfile()
+ 
+    const canEdit = staff.role === "ADMIN" || staff.role === "OWNER" 
+
+    const initialCours= await prisma.cours.findMany({
+        orderBy:{createdAt: "desc"}
+    })
+
     return(
         <main className="">
+            <CoursManager initialCours={initialCours}  canEdit={canEdit}/>
         </main>
     )
 }
