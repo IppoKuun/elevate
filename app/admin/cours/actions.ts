cfe84320a5c9ffa0b07cdf2b63dca3d05d6cf78f
Hippolyte <hippolyte.diallo@gmail.com>
2026-02-04 11:01:33 +0100

    feat(cours/action): add actionsUpdate

--- app/admin/cours/actions.ts
+++ app/admin/cours/actions.ts
@@ -1,7 +1,8 @@
 import { prisma } from "@/lib/db/prisma";
 import { requireStaffRole } from "@/lib/rbac";
-import { slugify } from "@/lib/validations";
+import { slugify, updateCourseSchema } from "@/lib/validations";
 import { CourSchema } from "@/lib/validations";
+import { object } from "better-auth";
 import { revalidatePath } from "next/cache";
 
 export async function generateUniqueSlug(title: string, id?:string){
@@ -21,7 +22,7 @@ export async function generateUniqueSlug(title: string, id?:string){
     }
 }
 
-export async function createCours(prevData: unknow, formData: FormData){
+export async function createCoursAction(prevData: unknow, formData: FormData){
     await requireStaffRole("ADMIN")
     const raw = Object.fromEntries(formData.entries())
     const parsed = CourSchema.safeParse({
@@ -37,8 +38,38 @@ export async function createCours(prevData: unknow, formData: FormData){
         data : {...parsed.data, slug : finalSlug},  
     });
     if (!create) return {ok:false, userMsg :" Impossible de créer le cours" }
-
+    return {ok:true}
 
     revalidatePath("/admin/cours")
+    
+}
+
+
+export async function updateCourseAction(prevData: unknow, formData: FormData){
+    await requireStaffRole("ADMIN")
+    const raw = Object.fromEntries(formData.entries())
+    const parsed = updateCourseSchema.safeParse({
+        priceCents: Number(raw.priceCents),
+        isPaid : raw.isPaid === "on"
+    })
+
+    if (!parsed.success) return {ok: false, error : parsed.error.flatten().fieldErrors}
+
+    const {id, ...updateData} = parsed.data
+
+    if (updateData.title !== prevData.title){
+        const updateData.slug = await generateUniqueSlug(updateData.title, id)
+    }
+
+    const updated = await prisma.cours.update({
+        where: {id} , data: updateData
+    })
+    if (!updated) return{
+        ok:false, userMsg: "Impossible d'enregistrer dans la base de données la modifications"
+    }
+    return {ok:true}
+}
+
+export async function deleteCoursAction(prevData:unknown, formData: FormData){
     
 }
