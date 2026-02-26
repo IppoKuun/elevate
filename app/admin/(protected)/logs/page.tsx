a431f10a796e9bfa7dea2252700d0e53fd201645
Hippolyte <hippolyte.devweb@gmail.com>
2026-02-26 12:15:39 +0100

    feat (audit.tsx) : start TSX logic

--- app/admin/(protected)/logs/page.tsx
+++ app/admin/(protected)/logs/page.tsx
@@ -1,12 +1,43 @@
+import { prisma } from "@/lib/db/prisma";
 import { requireStaffRole } from "@/lib/rbac";
-import { Router } from "lucide-react";
-import { redirect } from "next/dist/server/api-utils";
+import { AuditLog, Prisma } from "@prisma/client";
 import { useRouter } from "next/navigation";
-export default async function adminLogs() {
+
+interface pageParamsProps {
+    searchParams: Promise<{
+        page?: number,
+        type?: string,
+        staff?: string,
+        sort?: string
+    }>
+}
+
+export default async function adminLogs({searchParams} : pageParamsProps) {
+    const params = await searchParams
+
+    const page = Number(params.page)
+    const currentPage = Number(page ?? 1)
+    const type = params.type
+    const staff = params.staff
+    const sort = params.sort
+    const logsPerpages = 30
+
+    const where : Prisma.AuditLogWhereInput = {}
+    if (type) where.entityType = type
+    if(staff) where.actorAuthUserId = staff
+    const sortBy = sort === "asc" ? "desc"
+
     const Router = useRouter()
     const valid = await requireStaffRole("ADMIN")
     if (!valid){
         return Router.push("/admin/login")
-    }
+    } 
+
+    const [logs, logsCount] = await Promise.all([
+        prisma.auditLog.findMany({where, orderBy: {createdAt:"desc"}, 
+        include:{staffProfile}
+        }),
+        prisma.auditLog.count({where})
+    ])
     
 }
