d9fe0417feaeb69a780bb5aedee687d76b8180d8
Hippolyte <hippolyte.devweb@gmail.com>
2026-03-12 10:14:28 +0100

    fix (prismaConfig) : add directURL for migration

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ config()
 export default defineConfig({
   schema : "prisma/schema.prisma" ,
    datasource :{
-    url: process.env.DATABASE_URL!,
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL!,
    } ,  
    experimental:{
     externalTables : true
